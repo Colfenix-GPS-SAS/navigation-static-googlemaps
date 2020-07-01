fb90d9d536dc14b5448c8d5528f09f5262d12196
David Niño Herrán <45274593+davidnherran@users.noreply.github.com>
2020-06-30 21:25:19 -0500

    Configuración inicial + Renderizado de mapas

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+function initMap(){
+    var coord = {lat:-74.1483976 ,lng:4.5891058};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
